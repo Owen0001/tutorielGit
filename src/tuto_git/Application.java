1ffd7b29bbff18c9d1b3ab8f4af7f01d2f531e5e
Alexandre Sireix <asireix@pcalb-mm0000.univ-jfc.fr>
2020-02-05 11:54:22 +0100

    ajout titre fenetre

--- src/tuto_git/Application.java
+++ src/tuto_git/Application.java
@@ -20,6 +20,7 @@ public class Application extends Frame implements WindowListener{
 		modl.addObserver(vue);
 		vue.addActionListener(ctrl);
 		
+		this.setTitle("COMMUNISME");
 		this.addWindowListener(this);
 		this.add(vue);
 		this.pack();
